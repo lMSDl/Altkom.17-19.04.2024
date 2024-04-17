6248727b4920687e86125577a7d500b9c5968a4c
Paweł Łęczycki <pawel@mad.info.pl>
2024-04-17 12:25:29 +0200

    Łączenie stringów

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -24,4 +24,26 @@ string target = "World";
 Console.WriteLine(target);
 
 //zmiana wartości - każde kolejne przypisanie wartości
-helloVariable = "Hi";
+helloVariable = Console.ReadLine(); // Console.ReadLine() - pobranie danych z konsoli, zatwierdzamy enterem
+
+string output;
+
+//łączenie stringów za pomocą operatora +
+output = helloVariable + " " + target; // łączenie stringów to "wyprodukowanie" nowego stringa na podstawie połączenia innych
+
+Console.WriteLine(helloVariable);
+Console.WriteLine(target);
+Console.WriteLine(output);
+
+Console.WriteLine("Podaj imię:");
+string name = Console.ReadLine();
+
+string format = "{0} {1}!"; //wartości w nazwiasach oznaczają indeks parametru, który ma być wstawiony w to miejsce
+output = string.Format(format, helloVariable, name); //łączenie stringów za pomocą funkcji format
+Console.WriteLine(output);
+
+output = "{helloVariable} {name}!";
+Console.WriteLine(output);
+
+output = $"{helloVariable} {name}!"; //łączenie wykorzystujące interpolację (string interpolowany)
+Console.WriteLine(output);
