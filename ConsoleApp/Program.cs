e142f465d565a01516aeaa7d3e29c42f1a7e0de0
Paweł Łęczycki <pawel@mad.info.pl>
2024-04-17 12:08:29 +0200

    deklaracja, inicjalizacja, przypisywanie wartości

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿
+//wywołanie funkcji statycznej z klasy Console, która nie przyjmuje parametrów
+Console.WriteLine();
+
+
+//wywołanie funkcji statycznej z klasy Console z parametrem
 Console.WriteLine("Hello, World!");
+Console.Write("Hello, World!"); //WriteLine stawia znak nowej linii na koniec (enter), a funkcja Write - nie
+
+//deklaracja zmiennej o typie string (ciąg/łańcuch znaków) i nazwie helloVariable
+string helloVariable;
+
+//nie możemy używać niezainicjalizowanej zmiennej
+//Console.WriteLine(helloVariable);
+
+//inicjalizacja zmiennej - pierwsze przypisanie wartości
+helloVariable = "Hello";
+
+//przekazujemy naszą zmienną jako parametr funkcji
+Console.WriteLine(helloVariable);
+
+//deklaracja z inicjalizacją
+string target = "World";
+Console.WriteLine(target);
+
+//zmiana wartości - każde kolejne przypisanie wartości
+helloVariable = "Hi";
