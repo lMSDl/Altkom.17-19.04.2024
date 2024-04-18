1240ae927b1328cd793c7e83943e116f2dde828d
Paweł Łęczycki <pawel@mad.info.pl>
2024-04-18 11:53:17 +0200

    if

--- ConsoleApp/Demos.cs
+++ ConsoleApp/Demos.cs
@@ -9,6 +9,42 @@ namespace ConsoleApp
 {
     internal class Demos
     {
+        public void Lists()
+        {
+            //tworzymy nową listę. Lista po inicjalizacji jest pusta.
+            List<string> strings = new List<string>();
+            //List<string> strings = new ();
+
+            //dodajemy nowy element do listy, rozmiar tablicy się zwiększa
+            strings.Add("!");
+            strings.Add("ala");
+            strings.Add("kota");
+
+            //stawiamy element na konkretny indeks listy - pozostałe ementy przesuwają się
+            strings.Insert(2, "ma");
+
+
+            strings.Add("!");
+            strings.Add("!");
+
+            //usuwamy element pod indeksem 2 - rozmiar listy się zmniejsza
+            strings.RemoveAt(2);
+            //usuwamy element wg wartości - jeśli występuję więcej takich elementów, to usuwany jest pierwszy w kolejności
+            strings.Remove("!");
+
+            //w listach odwołujemy się do elementów po indeksach (tak jak w tablicach)
+            strings[strings.Count - 1] = "?";
+
+
+            Console.WriteLine(strings[1]);
+
+            //przekształcenie tablicy do listy
+            strings = Console.ReadLine().Split().ToList();
+
+
+            Console.WriteLine($"Lista ma rozmiar: {strings.Count}");
+        }
+
         public void Arrays()
         {
             int a = 5;
--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,39 +1,66 @@
 ﻿using ConsoleApp;
+using System.Xml;
 
 //Demos demo = new Demos();
 //demo.Strings();
 //demo.Numbers();
 //demo.Arrays();
-
-//tworzymy nową listę. Lista po inicjalizacji jest pusta.
-List<string> strings = new List<string>();
-//List<string> strings = new ();
-
-//dodajemy nowy element do listy, rozmiar tablicy się zwiększa
-strings.Add("!");
-strings.Add("ala");
-strings.Add("kota");
-
-//stawiamy element na konkretny indeks listy - pozostałe ementy przesuwają się
-strings.Insert(2, "ma");
+//demo.Lists();
 
 
-strings.Add("!");
-strings.Add("!");
+Console.WriteLine("Podaj bok kwadratu:");
+string input = Console.ReadLine();
 
-//usuwamy element pod indeksem 2 - rozmiar listy się zmniejsza
-strings.RemoveAt(2);
-//usuwamy element wg wartości - jeśli występuję więcej takich elementów, to usuwany jest pierwszy w kolejności
-strings.Remove("!");
+int side = int.Parse(input);
 
-//w listach odwołujemy się do elementów po indeksach (tak jak w tablicach)
-strings[strings.Count - 1] = "?";
+//if sprawdza warunek w nawiasie i jeśli jest on prawdziwy, to wykonuje się blok kodu pod nim
+if (side > 0)
+{
+    Console.WriteLine($"Kwadrat ma obwód {side * 4}");
+}
+// jeśli poprzedni warunek nie jest spełniony, to sprawdzany jest kolejny if
+// else if - może występować wielokrotne
+else if (side < 0)
+{
+    Console.WriteLine("Nie mogę policzyć obwodu z ujemnego rozmiaru");
+}
+//else - wykonuje blok kodu w każdym innym przypadku
+else /*if(side == 0)*/
+{
+    Console.WriteLine("Kwadrat nie istnieje");
+}
+
+//jeżeli używany else, to tylko jeden blok kodu zostanie wykonany i sprzwdzanie warunków zakończy się w przypadku wejścia w któryś z bloków
+//jeżeli nie używany else, to każdy if będzie traktowany osobno i warunek będzie sprzwdzany niezależnie
+//wniosek else jest też łącznikiem między kolejnymi if'ami
+
+if (side != 0)
+{
+    Console.WriteLine("Bok jest różny od 0");
+}
+if (side == 0)
+{
+    Console.WriteLine("Bok jest równy 0");
+}
+
+bool result = side == 0; //== - porównanie
+result = side != 0; //!= - nierówność
+result = side > 0; //> - większe
+result = side < 0; //< - mniejsze
+result = side >= 0; //>= - większe lub równe
+result = side <= 0; //<= - mniejsze lub równe
+
+result = side > 0 || side == 0; // || - logiczne lub (OR)
+result = side > 0 && side <= 10; // || - logiczne i (AND)
 
 
-Console.WriteLine(strings[1]);
+if (result)
+{
+    Console.WriteLine("Bok kwadratu jest z przedziału (0;10>");
+}
 
-//przekształcenie tablicy do listy
-strings = Console.ReadLine().Split().ToList();
-
-
-Console.WriteLine($"Lista ma rozmiar: {strings.Count}");
+//! - negacja - zaprzeczenie tego co występuje po wykrzykniku
+if (!result)
+{
+    Console.WriteLine("Bok kwadratu nie jest z przediały (0:10>");
+}
