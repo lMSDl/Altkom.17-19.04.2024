cb2e2b82d1f8a72c4755029d3cef7420a06f054e
Paweł Łęczycki <pawel@mad.info.pl>
2024-04-18 14:27:27 +0200

    while do-while

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -7,13 +7,54 @@ Demos demo = new Demos();
 //demo.Arrays();
 //demo.Lists();
 //demo.If();
-demo.Switch();
+//demo.Switch();
+
+
+bool stop = false;
+
+//while - pętla która trwa gdy jej parametr jest true, parametr jest sprawdzany przed każdym wejściem do pętli
+// jeśli przy pierwszym wejściu parametr będzie false, to pętla się nigdy nie wykona
+
+while (!stop)
+//while(true) = pętla nieskończona
+{
+
+    Console.WriteLine("Co chcesz zrobić?");
+    string operation = Console.ReadLine();
+
+    switch (operation)
+    {
+        case "exit":
+            stop = true;
+            break;
+        default:
+            Console.WriteLine(operation);
+            break;
+    }
+
+}
 
 
 
+bool finish;
+//do-while - sprawdza warunek po wykonaniu ciała - zapewnia, że zostanie ono wykonane co najmniej raz
+//pozwala to wyeliminować inicjalizację zminnych przed wejściem do pętli
+do
+{
+    Console.WriteLine("Co chcesz zrobić?");
+    string operation = Console.ReadLine();
+
+    switch (operation)
+    {
+        case "exit":
+            finish = true;
+            break;
+        default:
+            finish = false;
+            Console.WriteLine(operation);
+            break;
+    }
+} while(!finish);
 
 
-
-
-
-
+Console.WriteLine(  );
