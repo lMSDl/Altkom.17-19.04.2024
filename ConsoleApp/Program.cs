71e1e7b596939ea184f879402053b9df5582e9a9
Paweł Łęczycki <pawel@mad.info.pl>
2024-04-18 11:58:26 +0200

    TryParse (parametr wyjściowy)

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -11,56 +11,64 @@ using System.Xml;
 Console.WriteLine("Podaj bok kwadratu:");
 string input = Console.ReadLine();
 
-int side = int.Parse(input);
-
-//if sprawdza warunek w nawiasie i jeśli jest on prawdziwy, to wykonuje się blok kodu pod nim
-if (side > 0)
+int side;
+bool parseSuccess = int.TryParse(input, out side);
+if (!parseSuccess)
 {
-    Console.WriteLine($"Kwadrat ma obwód {side * 4}");
+    Console.WriteLine("Błędna wartość!");
 }
-// jeśli poprzedni warunek nie jest spełniony, to sprawdzany jest kolejny if
-// else if - może występować wielokrotne
-else if (side < 0)
+else
 {
-    Console.WriteLine("Nie mogę policzyć obwodu z ujemnego rozmiaru");
-}
-//else - wykonuje blok kodu w każdym innym przypadku
-else /*if(side == 0)*/
-{
-    Console.WriteLine("Kwadrat nie istnieje");
-}
 
-//jeżeli używany else, to tylko jeden blok kodu zostanie wykonany i sprzwdzanie warunków zakończy się w przypadku wejścia w któryś z bloków
-//jeżeli nie używany else, to każdy if będzie traktowany osobno i warunek będzie sprzwdzany niezależnie
-//wniosek else jest też łącznikiem między kolejnymi if'ami
+    //if sprawdza warunek w nawiasie i jeśli jest on prawdziwy, to wykonuje się blok kodu pod nim
+    if (side > 0)
+    {
+        Console.WriteLine($"Kwadrat ma obwód {side * 4}");
+    }
+    // jeśli poprzedni warunek nie jest spełniony, to sprawdzany jest kolejny if
+    // else if - może występować wielokrotne
+    else if (side < 0)
+    {
+        Console.WriteLine("Nie mogę policzyć obwodu z ujemnego rozmiaru");
+    }
+    //else - wykonuje blok kodu w każdym innym przypadku
+    else /*if(side == 0)*/
+    {
+        Console.WriteLine("Kwadrat nie istnieje");
+    }
 
-if (side != 0)
-{
-    Console.WriteLine("Bok jest różny od 0");
-}
-if (side == 0)
-{
-    Console.WriteLine("Bok jest równy 0");
-}
+    //jeżeli używany else, to tylko jeden blok kodu zostanie wykonany i sprzwdzanie warunków zakończy się w przypadku wejścia w któryś z bloków
+    //jeżeli nie używany else, to każdy if będzie traktowany osobno i warunek będzie sprzwdzany niezależnie
+    //wniosek else jest też łącznikiem między kolejnymi if'ami
 
-bool result = side == 0; //== - porównanie
-result = side != 0; //!= - nierówność
-result = side > 0; //> - większe
-result = side < 0; //< - mniejsze
-result = side >= 0; //>= - większe lub równe
-result = side <= 0; //<= - mniejsze lub równe
+    if (side != 0)
+    {
+        Console.WriteLine("Bok jest różny od 0");
+    }
+    if (side == 0)
+    {
+        Console.WriteLine("Bok jest równy 0");
+    }
 
-result = side > 0 || side == 0; // || - logiczne lub (OR)
-result = side > 0 && side <= 10; // || - logiczne i (AND)
+    bool result = side == 0; //== - porównanie
+    result = side != 0; //!= - nierówność
+    result = side > 0; //> - większe
+    result = side < 0; //< - mniejsze
+    result = side >= 0; //>= - większe lub równe
+    result = side <= 0; //<= - mniejsze lub równe
+
+    result = side > 0 || side == 0; // || - logiczne lub (OR)
+    result = side > 0 && side <= 10; // || - logiczne i (AND)
 
 
-if (result)
-{
-    Console.WriteLine("Bok kwadratu jest z przedziału (0;10>");
-}
+    if (result)
+    {
+        Console.WriteLine("Bok kwadratu jest z przedziału (0;10>");
+    }
 
-//! - negacja - zaprzeczenie tego co występuje po wykrzykniku
-if (!result)
-{
-    Console.WriteLine("Bok kwadratu nie jest z przediały (0:10>");
+    //! - negacja - zaprzeczenie tego co występuje po wykrzykniku
+    if (!result)
+    {
+        Console.WriteLine("Bok kwadratu nie jest z przediały (0:10>");
+    }
 }
