b121205d211bb2a771d9152fda7c88b45d481004
Paweł Łęczycki <pawel@mad.info.pl>
2024-04-19 13:42:07 +0200

    Konstruktory cd.

--- Demo/Product.cs
+++ Demo/Product.cs
@@ -4,6 +4,13 @@ namespace Demo
 {
     internal class Product
     {
+        public Product(string name, float price)
+        {
+            Name = name;
+            Price = price;
+        }
+
+
         public string Name { get; set; }
         public float Price { get; set; }
 
--- Demo/Program.cs
+++ Demo/Program.cs
@@ -7,8 +7,9 @@ int count = int.Parse(Console.ReadLine());
 Product[] products = new Product[count];
 for (int i = 0; i < count; i++)
 {
-    products[i] = new Product();
-    FillProduct(products[i]);
+    /*products[i] = new Product();
+    FillProduct(products[i]);*/
+    products[i] = CreateProduct();
 }
 
 
@@ -17,9 +18,22 @@ foreach (Product product in products)
     Console.WriteLine(product.GetDescription());
 }
 
-Product myProduct = new Product();
-FillProduct(myProduct);
+Product myProduct = CreateProduct();
+Console.WriteLine(myProduct.GetDescription());
 
+Product CreateProduct()
+{
+    Console.WriteLine("Podaj nazwę produktu:");
+    string name = Console.ReadLine();
+
+    Console.WriteLine("Podaj cenę:");
+    string priceString = Console.ReadLine();
+    float price = float.Parse(priceString);
+
+
+    Product product = new Product(name, price);
+    return product;
+}
 
 void FillProduct(Product productToFill)
 {
