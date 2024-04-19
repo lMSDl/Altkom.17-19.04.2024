e0aa61f69ff7bf97e3a6cb99b3c4fe06553bb332
Paweł Łęczycki <pawel@mad.info.pl>
2024-04-19 12:19:06 +0200

    metody z parametrem

--- Demo/Product.cs
+++ Demo/Product.cs
@@ -1,8 +1,29 @@
-﻿namespace Demo
+﻿using System;
+
+namespace Demo
 {
     internal class Product
     {
         public string Name { get; set; }
         public float Price { get; set; }
+
+        public string GetDescription()
+        {
+            string description = $"{Name} - {Price}zł";
+            return description;
+        }
+
+
+        /*public void FillProduct()
+        {
+            Console.WriteLine("Podaj nazwę produktu:");
+            Name = Console.ReadLine();
+
+
+            Console.WriteLine("Podaj cenę:");
+            string priceString = Console.ReadLine();
+            float price = float.Parse(priceString);
+            Price = price;
+        }*/
     }
 }
--- Demo/Program.cs
+++ Demo/Program.cs
@@ -1,15 +1,34 @@
 ﻿using Demo;
 using System;
 
+Console.WriteLine("Ile produktów chcesz utworzyć?");
+int count = int.Parse(Console.ReadLine());
+
+Product[] products = new Product[count];
+for (int i = 0; i < count; i++)
+{
+    products[i] = new Product();
+    FillProduct(products[i]);
+}
+
+
+foreach (Product product in products)
+{
+    Console.WriteLine(product.GetDescription());
+}
+
 Product myProduct = new Product();
-
-Console.WriteLine("Podaj nazwę produktu:");
-myProduct.Name = Console.ReadLine();
+FillProduct(myProduct);
 
 
-Console.WriteLine("Podaj cenę:");
-string priceString = Console.ReadLine();
-float price = float.Parse(priceString);
-myProduct.Price = price;
+void FillProduct(Product productToFill)
+{
+    Console.WriteLine("Podaj nazwę produktu:");
+    productToFill.Name = Console.ReadLine();
 
-Console.WriteLine($"{myProduct.Name} - {myProduct.Price}zł");
+
+    Console.WriteLine("Podaj cenę:");
+    string priceString = Console.ReadLine();
+    float price = float.Parse(priceString);
+    productToFill.Price = price;
+}
