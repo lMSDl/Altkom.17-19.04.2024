ff417fca56731666c2f8cef087e649cb6a8ee101
Paweł Łęczycki <pawel@mad.info.pl>
2024-04-19 09:32:29 +0200

    foreach

--- ConsoleApp/Demos.cs
+++ ConsoleApp/Demos.cs
@@ -9,6 +9,56 @@ namespace ConsoleApp
 {
     internal class Demos
     {
+
+        public void For()
+        {
+            //I - inicjalizacja pętli - wykonuje się tylko raz na początku
+            //II - warunek kontynuacji pętli - wykonuje się przed każdym wykonaniem ciała
+            //III - ciało pętli
+            //IV - akcja po wykonaniu ciała - najczęsciej inkrementacja licznika
+            //for(I ; II ; IV)
+            //{ III }
+
+            for (int i = 0; i < 5; i++ /*i = i + 1*/)
+            {
+                Console.WriteLine(i);
+            }
+
+
+            //pętla nieskończona - odpowiednik while(true)
+            /*for(; ; )
+            {
+                Console.Write("*");
+            }*/
+
+            int value = 5;
+            for (; value > 0; value--/*value = value - 1*/)
+            {
+                Console.WriteLine(value);
+            }
+            Console.WriteLine("---");
+            value = 0;
+            for (; value < 5;)
+            {
+                Console.WriteLine(++value); //wartość wzrasta przed wyświetleniem
+            }
+
+            value = 0;
+            for (; value < 5;)
+            {
+                Console.WriteLine(value++); //wartość wzrasta po wyświetleniu
+            }
+
+
+            string input = Console.ReadLine();
+            string[] splittedInput = input.Split();
+
+            for (int i = splittedInput.Length - 1; i >= 0; i--)
+            {
+                Console.WriteLine(splittedInput[i]);
+            }
+        }
+
         public void DoWhile()
         {
             bool finish;
--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -7,51 +7,47 @@ Demos demo = new Demos();
 //demo.Arrays();
 //demo.Lists();
 //demo.If();
-//demo.Switch();
+//demo.Switch
+//demo.For();
 
 
-//I - inicjalizacja pętli - wykonuje się tylko raz na początku
-//II - warunek kontynuacji pętli - wykonuje się przed każdym wykonaniem ciała
-//III - ciało pętli
-//IV - akcja po wykonaniu ciała - najczęsciej inkrementacja licznika
-//for(I ; II ; IV)
-//{ III }
-
-for (int i = 0; i < 5; i++ /*i = i + 1*/)
+float[] numbers;
+bool exit;
+do
 {
-    Console.WriteLine(i);
+    exit = true;
+    Console.WriteLine("Wypisz jakieś liczby:");
+    string input = Console.ReadLine();
+
+    string[] splittedInput = input.Split();
+
+    numbers = new float[splittedInput.Length];
+
+    for (int i = 0; i < splittedInput.Length; i++)
+    {
+        float value;
+        if (float.TryParse(splittedInput[i], out value))
+        {
+            numbers[i] = value;
+        }
+        else
+        {
+            Console.WriteLine("Błędne dane");
+            exit = false;
+            break; //przerywa wykonywanie pętli
+        }
+    }
+} while (!exit);
+
+for (int i = 0; i < numbers.Length; i++)
+{
+    float number = numbers[i];
+    Console.Write(number);
 }
 
-
-//pętla nieskończona - odpowiednik while(true)
-/*for(; ; )
+//foreach zastępuje powyższy sposób iteracji po tablicy
+//foreach - pozwala przejsc po wszystkich elementach tablicy
+foreach (float number in numbers)
 {
-    Console.Write("*");
-}*/
-
-int value = 5;
-for (; value > 0; value--/*value = value - 1*/)
-{
-    Console.WriteLine( value );
-}
-Console.WriteLine("---");
-value = 0;
-for (; value < 5;)
-{
-    Console.WriteLine(++value); //wartość wzrasta przed wyświetleniem
-}
-
-value = 0;
-for (; value < 5;)
-{
-    Console.WriteLine(value++); //wartość wzrasta po wyświetleniu
-}
-
-
-string input = Console.ReadLine();
-string[] splittedInput = input.Split();
-
-for (int i = splittedInput.Length - 1; i >= 0; i--)
-{
-    Console.WriteLine(splittedInput[i]);
+    Console.Write(number);
 }
